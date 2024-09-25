e29fb9c5323a7de44e66d1467ed6604e39a037f3
Willem Boone <willem.boone@vliz.be>
2024-09-25 11:08:58 +0200

    change prefix '' to undefined

--- next.config.js
+++ next.config.js
@@ -4,5 +4,5 @@ const isDev =
   process.env.VERCEL_ENV === 'preview' || process.env.NODE_ENV === 'development'
 
 module.exports = {
-  assetPrefix: isDev ? '' : 'https://seaweed-farming.carbonplan.org',
+  assetPrefix: isDev ? undefined : undefined,
 }
